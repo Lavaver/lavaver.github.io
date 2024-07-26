ebd7e7d2212126318c2f7a4b05224c87239afae6
Lavaver <yansuan_game@qq.com>
2024-07-26 15:57:24 +0800

    Site updated: 2024-07-26 15:57:19

--- js/diaspora.js
+++ js/diaspora.js
@@ -615,6 +615,7 @@ $(function() {
 					  owner: comment.data('o'),
 					  admin: comment.data('a'),
 					  id: decodeURI(window.location.pathname),
+                      proxy: 'https://talk.x-ipa.online/https://github.com/login/oauth/access_token',
 					  distractionFreeMode: comment.data('d')
 					})
 					$(".comment").removeClass("link")
