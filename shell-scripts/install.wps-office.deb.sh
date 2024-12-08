159f10bf01a22985450f566bc56288921f5df592
Lavaver <luozicat@outlook.com>
2024-12-08 20:58:36 +0800

    站点已部署于 20:58:36

---
+++ shell-scripts/install.wps-office.deb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Install WPS Office
+@echo off
+echo "Updating APT package list..."
+sudo apt update
+echo "Installing WPS Office..."
+sudo apt install ./1_rZyXFwPS_wps-office_12.1.0.17900_amd64.deb
